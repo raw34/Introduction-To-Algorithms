290d7d556c10bba8b1e422408ed40505a4c565a0
raw34 <raw34@sina.com>
2013-12-13 07:45:54 +0800

    update

--- charpter2/merge_sort.php
+++ charpter2/merge_sort.php
@@ -2,11 +2,6 @@
 function merge_sort(&$a, $p, $r) {
     if ($p < $r) {
         $q = ($p + $r) / 2;
-        /*$a = merge_sort($a, $p, $q);
-        $a = merge_sort($a, $q + 1, $r);
-        $a = merge($a, $p, $q, $r);
-        return $a;
-         */
         merge_sort($a, $p, $q);
         merge_sort($a, $q + 1, $r);
         merge($a, $p, $q, $r);
@@ -17,29 +12,32 @@ function merge(&$a, $p, $q, $r) {
     $n1 = $q - $p + 1;
     $n2 = $r - $q;
     //echo '<br/>', $n1, '=', $n2;
-    $left = array();
-    $right = array();
+    $left = range(0, $n1);
+    $right = range(0, $n2);
     for ($i = 0; $i < $n1; $i++) {
         $left[$i] = $a[$p + $i];
     }
     for ($j = 0; $j < $n2; $j++) {
         $right[$j] = $a[$q + $j + 1];
     }
+    #echo '<pre>'; var_dump($left); echo '</pre>';
     $left[$n1] = $right[$n2] = PHP_INT_MAX;
     $i = $j = 0;
     for ($k = $p; $k < $r; $k++) {
+        echo $k, "\n";
         if ($left[$i] <= $right[$j]) {
-            $a[$k] = $left[$i++];     
+            $a[$k] = $left[$i];     
+            $i++;
         } else {
-            $a[$k] = $right[$j++];
+            $a[$k] = $right[$j];
+            $j++;
         }
     }
-    //return $a;
 }
 
 $arr = range(1, 10);
 shuffle($arr);
 echo '<pre>'; var_dump($arr); echo '</pre>';
-$arr = merge_sort($arr, 0, 10);
+$arr = merge_sort($arr, 0, 9);
 echo '<pre>'; var_dump($arr); echo '</pre>';
 ?>
