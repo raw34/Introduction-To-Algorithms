8b7e4393397c50a25591527e5c56864f4419587a
raw34 <raw34@sina.com>
2013-12-25 08:59:57 +0800

    update

--- charpter8/bucket_sort.php
+++ charpter8/bucket_sort.php
@@ -1,8 +1,9 @@
 <?php 
 require '../charpter2/insertion_sort.php';
 
-function bucket_sort($a) {
+function bucket_sort($a, $max) {
     $n = count($a);
+    $m = $n * $max + 1;
     $buckets = array();
     for ($i = 0; $i < $n; $i++) {
         $j = floor($n * $a[$i]);
@@ -10,9 +11,8 @@ function bucket_sort($a) {
         $buckets[$j] = !isset($buckets[$j]) ? array() : $buckets[$j];
         array_push($buckets[$j], $a[$i]);
     }
-    //echo '<pre>'; var_dump($buckets); echo '</pre>';
     $b = array();
-    for ($i = 0; $i < $n; $i++) {
+    for ($i = 0; $i < $m; $i++) {
         //echo '<br/>$i =', $i;
         if (isset($buckets[$i])) {
             //echo ' bucket = ', implode(',', $buckets[$i]);
@@ -30,6 +30,6 @@ $arr1 = array_merge($arr1, $arr2);
 //$arr1 = range(1, 10);
 shuffle($arr1);
 echo '<pre>'; var_dump($arr1); echo '</pre>';
-$arr1 = bucket_sort($arr1);
+$arr1 = bucket_sort($arr1, max($arr1));
 echo '<pre>'; var_dump($arr1); echo '</pre>';
 ?>
